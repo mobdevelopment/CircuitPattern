497702322b76f689fb575983550f5f9ebe48a754
Michael Schouten <manschouten@hotmail.com>
2016-06-01 14:57:27 +0200

    initial commit basic screen done

---
+++ DesignPatterns1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace DesignPatterns1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("You clicked 'File'");
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true) { }
+               // txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+
+        private void ExitProgram(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("You clicked 'Exit'");
+        }
+    }
+}
